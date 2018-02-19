a5da94dc532cfb7f5d4fbc8312d68343f0ea8de2
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2018-02-19 09:25:14 +0100

    getQemyByVmId was replaced by Qemu.getByVmId

--- proxmox.qemu/index.js
+++ proxmox.qemu/index.js
@@ -8,9 +8,10 @@ module.exports = (NODE) => {
   qemuOut.on('trigger', (conn, state, callback) => {
     nodeIn.getValues(state)
     .then((nodes) => {
-      Promise.all(nodes
-      .filter(node => !!node)
-      .map(node => node.getQemuByVmId(NODE.data.vmId))
+      Promise.all(
+        nodes
+        .filter(node => !!node)
+        .map(node => node.Qemu.getByVmId(NODE.data.vmId))
       )
       .then((qemus) => {
         callback(qemus);
